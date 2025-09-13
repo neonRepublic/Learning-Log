26331f1029975baa0f9384a4ef08d290cf0c72a2
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-09-13 18:41:04 -0400

    added my app to default list of INSTALLED_APPS in settings folder

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my learning log app
+    'learning_logs',
+    #default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
