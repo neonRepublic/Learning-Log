e94c20134918ff49b143a47225155857cd541df1
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-09-16 19:51:19 -0400

    added form for users to add topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect 
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request): 
     """The home page for Learning Log.""" 
@@ -18,3 +19,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.xhtml', context)
+
+def new_topic(request):
+    """adds new topic"""
+    if request.method != 'POST':
+        #no data submitted then create blank screen
+        form = TopicForm()
+
+    else: #POST data submitted then process
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    #displays a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.xhtml', context)
+            
