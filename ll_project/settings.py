9f85079d2c6614add619961e93f3e72b1d57bd9f
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-09-16 22:51:23 -0400

    login issues fixed

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my learning log app
     'learning_logs',
+    'accounts',
     #default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings. 
+LOGIN_REDIRECT_URL = 'learning_logs:index'
