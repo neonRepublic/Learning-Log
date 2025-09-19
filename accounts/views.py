30563e396a6bd73f07c46390564253924b0e2054
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-09-19 18:52:07 -0400

    logout functionality added, login tested and passed, project completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """register new user"""
+    if request.method != 'POST':
+        #display blank registration form
+        form = UserCreationForm()
+    else: 
+        #process complete form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #log the user in then redirect to Home
+            login(request, new_user)
+            return redirect('learning_logs:index')
+        
+    #display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'registration/register.xhtml', context)
+
+
+
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404 
-
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -7,15 +8,21 @@ def index(request):
     """The home page for Learning Log.""" 
     return render(request, 'learning_logs/index.xhtml')
 
+@login_required
 def topics(request):
     """Show all topics."""
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     topics = Topic.objects.order_by('date_added')
     context = {"topics": topics}
     return render(request, 'learning_logs/topics.xhtml', context)
 
+@login_required 
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = get_object_or_404(Topic, id=topic_id)
+    # Make sure the topic belongs to the current user. 
+    if topic.owner != request.user: 
+        raise Http404
     
     # --- DEBUGGING LINE ---
     print(f"DEBUG: Retrieved topic with ID {topic.id} and name '{topic.text}'")
@@ -25,6 +32,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.xhtml', context)
 
+@login_required 
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -34,13 +42,17 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False) 
+            new_topic.owner = request.user 
+            new_topic.save()
+            # form.save()
             return redirect('learning_logs:topics')
     
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.xhtml', context)
 
+@login_required 
 def new_entry(request, topic_id):
     """Add a new entry for a topic."""
     topic = get_object_or_404(Topic, id=topic_id)
@@ -61,10 +73,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.xhtml', context)
 
+@login_required 
 def edit_entry(request, entry_id):
     """edit an exiting entry"""
     entry= Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user: 
+        raise Http404
 
     if request.method != 'POST':
         #initial request to prefill form with current entry
