3e9cd72f1da76decd114598c6558de9bb08db69a
Shaun <194363997+neonRepublic@users.noreply.github.com>
2025-09-19 19:09:11 -0400

    fixed layer 8 issue that caused problem with logging out and the views file where error would display after trying to add a new entry, re-migrated files in shell and now works as intended

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -12,7 +12,6 @@ def index(request):
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    topics = Topic.objects.order_by('date_added')
     context = {"topics": topics}
     return render(request, 'learning_logs/topics.xhtml', context)
 
@@ -66,6 +65,7 @@ def new_entry(request, topic_id):
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
+            new_entry.owner = request.user
             new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
     
